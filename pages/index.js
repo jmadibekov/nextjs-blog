c88c894158a2507b1162756337c831d81bceccd4
Nurbakyt Madibek <madibekov.nurbakhyt@gmail.com>
2021-05-02 17:07:03 +0600

    add dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,6 @@ export default function Home({ allPostsData }) {
         <p>
           Why, hello there! My name is <b>Joe</b> and I love Next.js!
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
